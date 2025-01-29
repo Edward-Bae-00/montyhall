2d80050ba4cc90daeca5ea5e5e9fcc7ffd091243
edward <145471329+Edward-Bae-00@users.noreply.github.com>
2025-01-29 12:13:14 -0500

    Completed Simulation of Monty Hall

--- monty.java
+++ monty.java
@@ -1,32 +1,37 @@
 import java.util.*;
 
 public class monty{
-    int right = 0;
-    int wrong = 0;
-
     public static void main(String [] args) throws Exception{
         // Scanner reader = new Scanner("thing.txt");
         // System.out.println("Enter how many simulations you'd like: ");
         // int n = reader.nextInt();
 
-        
-        int car = (int)(Math.random()*3);
-        int choose = (int)(Math.random()*3);
+        int right = 0;
+        int wrong = 0;
 
-        List<Integer> possible = new ArrayList<>();
-        for (int a = 0; a < 3; a++){
-            if (a != choose){
-                possible.add(a);
+        for(int a = 0; a < 100000; a++){
+            int car = (int)(Math.random()*3);
+            int choose = (int)(Math.random()*3);
+    
+            List<Integer> possible = new ArrayList<>();
+            for (int b = 0; b < 3; b++){
+                if (b != choose){
+                    if (b != car){
+                        possible.add(b);
+                    }
+                }
             }
+            if (possible.size() == 1){
+                right++;
+            }else{
+                possible.remove((int)(Math.random()*2));
+                if (possible.get(0) == car){
+                    right++;
+                }else{
+                    wrong++;
+                }
+            }
+            System.out.println("Right: "+ right + " Wrong: " + wrong);
         }
-        
-        int hint = (int)(Math.random()*2);
-        
-        
-
-
-        System.out.println("car: " + car);
-        System.out.println("picked pos: " + choose);
-
     }
 }
